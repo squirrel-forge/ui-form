558d670b6d24fb14010694b1674cfe523421122b
dhartwell <d.hartwell@sekeido.com>
2022-04-24 19:57:46 +0200

    update index

--- src/es6/index.js
+++ src/es6/index.js
@@ -11,6 +11,7 @@
  * Form
  */
 export { FormValues } from './Form/FormValues.js';
+export { Html5Validator } from './Form/Html5Validator.js';
 export { UiFormComponent } from './Form/UiFormComponent.js';
 
 /**
@@ -20,4 +21,5 @@ export { UiFormPluginValues } from './Plugins/UiFormPluginValues.js';
 export { UiFormPluginPrefetch } from './Plugins/UiFormPluginPrefetch.js';
 export { UiFormPluginJSONResponse } from './Plugins/UiFormPluginJSONResponse.js';
 export { UiFormPluginFieldControl } from './Plugins/UiFormPluginFieldControl.js';
+export { UiFormPluginValidate } from './Plugins/UiFormPluginValidate.js';
 export { UiFormPluginReCaptcha } from './Plugins/UiFormPluginReCaptcha.js';
