87a5dd4d92aa9ea5c54b9e77fd4b6d6e3c9648d3
dasiux <me@siux.info>
2022-06-09 21:37:01 +0200

    update version

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,6 +1,6 @@
 /* !
  * @module      : @squirrel-forge/ui-form
- * @version     : 0.9.1
+ * @version     : 0.9.2
  * @license     : MIT
  * @copyright   : 2022 squirrel-forge
  * @author      : Daniel Hartwell aka. siux <me@siux.info>
